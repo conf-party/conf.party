3dd0d9f6fd620cb50450c735546e2af4eb0f501a
Marcus Noble <github@marcusnoble.co.uk>
2023-03-13 18:38:27 +0000

    Kubecon EU 2023
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

---
+++ src/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Conference struct {
+	Filename string
+	Name     string
+	Date     string
+	Website  string
+	Location string
+	Parties  []Party
+}
+
+type Party struct {
+	Name        string
+	Date        string
+	Website     string
+	Location    string
+	Description string
+	Notes       string
+}
+
+func main() {
+	srcManifests, err := ioutil.ReadDir("../conferences")
+	if err != nil {
+		panic(err)
+	}
+
+	indexTmpl := template.Must(template.ParseFiles("./templates/index.html"))
+	eventTmpl := template.Must(template.ParseFiles("./templates/event.html"))
+
+	confs := []Conference{}
+
+	for _, file := range srcManifests {
+		if !file.IsDir() {
+			data, err := ioutil.ReadFile(path.Join("../conferences", file.Name()))
+			if err != nil {
+				log.Println(err)
+				return
+			}
+
+			var conf Conference
+
+			err = yaml.Unmarshal(data, &conf)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			conf.Filename = strings.ReplaceAll(file.Name(), ".yaml", ".html")
+
+			confs = append(confs, conf)
+		}
+	}
+
+	for _, conf := range confs {
+		outFile, err := os.Create(path.Join("../out", conf.Filename))
+		if err != nil {
+			log.Println("create file: ", err)
+			return
+		}
+
+		if err := eventTmpl.Execute(outFile, conf); err != nil {
+			log.Println("template event: ", err)
+		}
+	}
+
+	outFile, err := os.Create(path.Join("../out", "index.html"))
+	if err != nil {
+		log.Println("create file: ", err)
+		return
+	}
+
+	if err := indexTmpl.Execute(outFile, confs); err != nil {
+		log.Println("template event: ", err)
+	}
+}
+
+const dateLayout = "2006-01-02"
+
+func (c Conference) PrettyDate() string {
+	t, _ := time.Parse(dateLayout, c.Date)
+	return t.Format("Monday, 2 January")
+}
+
+func (p Party) PrettyDate() string {
+	t, _ := time.Parse(dateLayout, p.Date)
+	return t.Format("Monday, 2 January")
+}
